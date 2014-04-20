f449429c203c0e64e12385d0b1f34804256a88b7
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-20 11:47:59 -0700

    added rescue if SerialPort cant be established

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,22 +9,26 @@ class DashboardController < ApplicationController
     data_bits = 8
     stop_bits = 1
     parity = SerialPort::NONE
-     
-    port = SerialPort.new(port_file, baud_rate, data_bits, stop_bits, parity)
-     
-    port.write "b"
 
-    values_string = ""
-    port_line = ""
-    i = 1
+    begin
+      port = SerialPort.new(port_file, baud_rate, data_bits, stop_bits, parity)
 
-    until port_line.include? "end"
-      port_line = port.readline(i)
-      values_string += port_line
-      i += 1
+      port.write "b"
+
+      values_string = ""
+      port_line = ""
+      i = 1
+
+      until port_line.include? "end"
+        port_line = port.readline(i)
+        values_string += port_line
+        i += 1
+      end
+
+      values_array = values_string.split("\r\n")
+    rescue
+      values_array = ["no values received. please make sure device is plugged in."]
     end
-
-    values_array = values_string.split("\r\n")
 
     respond_to do |f|
       f.json { render :json=> values_array }
