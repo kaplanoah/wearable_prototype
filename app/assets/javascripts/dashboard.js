e664fe47a85b7e7973e1a30ed6b3a189823b3934
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-20 16:16:37 -0700

    connected graph with data, displaying values when import button is clicked

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,36 @@
-importValues = function(){
-  $.get('/import_values.json').done(function(data){
-    $('#dashboard').empty()
-    $('#dashboard').append(data);
-  });
-}
+$(document).ready(function(){
+
+  var ctx = $("#graph").get(0).getContext("2d");
+
+  var blankLabels = [];
+  for (var i = 0; i < 50; i++) {
+    blankLabels.push("");
+  }
+
+  $('#import').click(importValues);
+
+  function importValues(){
+    $.get('/import_values.json').done(function(data){
+      drawGraph(data);
+    });
+  }
+
+  function drawGraph(values){
+
+    var data = {
+      labels : blankLabels,
+      datasets : [
+        {
+          fillColor   : "red",
+          strokeColor : "darkred",
+          pointColor  : "white",
+          pointStrokeColor : "grey",
+          data : values
+        }
+      ]
+    }
+
+    var graph = new Chart(ctx).Line(data);
+  }
+
+});
